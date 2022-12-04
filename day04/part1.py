bebba3dd43433cf58134400a4daf6abff13ce067
Dylan Goodman <dylan.goodman60@gmail.com>
2022-12-04 13:03:57 -0500

    Day  4

---
+++ day04/part1.py
@@ -0,0 +1,13 @@
+f = open("input.txt")
+
+total = 0
+for line in f.read().split("\n"):
+    p1, p2 = line.split(",")
+    e1, e2 = p1.split("-"), p2.split("-")
+
+    if int(e1[0]) >= int(e2[0]) and int(e1[1]) <= int(e2[1]):
+        total += 1
+    elif int(e2[0]) >= int(e1[0]) and int(e2[1]) <= int(e1[1]):
+        total += 1
+
+print(total)
